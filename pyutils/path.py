2577d310ac6ed3be9c03518b18d3f32e48ba2a76
Brandon Braun <brandonbraun653@gmail.com>
2021-04-16 21:08:04 -0500

    Add a useful path walking function

---
+++ pyutils/path.py
@@ -0,0 +1,28 @@
+# **********************************************************************************************************************
+#   FileName:
+#       path.py
+#
+#   Description:
+#       Utilities for interacting with paths
+#
+#   4/16/21 | Brandon Braun | brandonbraun653@gmail.com
+# **********************************************************************************************************************
+
+from pathlib import Path
+
+
+def find_parent_path(current: Path, parent: str) -> Path:
+    """
+    Recursively finds the desired root path
+
+    Args:
+      current (Path): Starting path to search from
+      parent (str): Stem inside the path
+
+    Returns:
+      Path: The found path or the system drive mount point
+    """
+    if current.stem == parent or current.is_mount():
+        return current
+    else:
+        return find_parent_path(current.parent, parent)
